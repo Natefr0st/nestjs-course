cc96ed380c6ff85f74b144f1ded58b7d13e5665c
Kevin <kevin_zino@yahoo.com>
2024-02-11 02:07:58 +0200

    Add sign up logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import * as argon from 'argon2';
 
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup(dto: AuthDto) {
+  async signup(dto: AuthDto) {
     // generate password hash
-
+    const hash = await argon.hash(dto.password);
     // save the new user in the db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-    // return the saved user
-    return { msg: 'I have signed up' };
+      delete user.hash;
+
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already exists');
+        }
+      }
+
+      throw error;
+    }
   }
 
   signin() {
